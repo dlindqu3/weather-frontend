75ba714ac8decafaffe2a066653e037ec9a557ce
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-10-31 18:10:25 -0400

    got weather in fahrenheit

--- src/pages/WeatherCharts.js
+++ src/pages/WeatherCharts.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState } from "react"
+import axios from "axios"
 
 function WeatherCharts() {
-
+  const [hasInput, setHasInput] = useState(false)
   const [locsQuery, setLocsQuery] = useState()
   const [locsArr, setLocsArr] = useState()
-  const [isLoading, setIsLoading] = useState(false);
+  const [finalLoc, setFinalLoc] = useState()
+  const [isLoading, setIsLoading] = useState(false)
 
   let baseURL = "http://localhost:4500/api"
   // http://localhost:4500/api/location/boston
@@ -13,36 +14,76 @@ function WeatherCharts() {
 
   const handleLocationsSubmit = async (e) => {
     e.preventDefault()
-    setIsLoading(true) 
+    setIsLoading(true)
     let queryUrl = baseURL + "/location/" + locsQuery
     let resData = await axios.get(queryUrl)
     let resArr = resData.data
+    
     setLocsArr(resArr)
     setIsLoading(false)
+    setHasInput(true)
+  };
+
+  const handleLocChange = (e) => {
+    console.log(e.target.value)
+    setFinalLoc(e.target.value)
   }
 
+  const handleCallWeather = async (coordinates) => {
+    let queryUrl = baseURL + "/weather/coordinates/" + coordinates[0] + "/" + coordinates[1]
+    console.log('queryUrl: ', queryUrl)
+    let resData = await axios.get(queryUrl)
+    console.log(resData.data.list)
+  }
+
+  const findCoordinates = () => {
+    for (let i = 0; i < locsArr.length; i++){
+      if (locsArr[i][0] === finalLoc){
+        let coordinates = [locsArr[i][1], locsArr[i][2]]
+        return coordinates
+      }
+    }
+  }
+
+  const handleLocSubmit = async (e) => {
+    e.preventDefault()
+    let coordinates = findCoordinates()
+    handleCallWeather(coordinates)
+  };
 
   return (
     <div>
       <p>Weather Charts here</p>
-      <p>add components for: query lat/lon, display chart</p>
-      <p>This page will be restricted to logged-in users</p>
+      {/* <p>add components for: query lat/lon, display chart</p>
+      <p>This page will be restricted to logged-in users</p> */}
 
-      <form onSubmit={handleLocationsSubmit}>
-        <input
-          type="text"
-          placeholder="City..."
-          onChange={(e) => setLocsQuery(e.target.value)}
-        />
-      </form>
+      {!hasInput && (
+        <form onSubmit={handleLocationsSubmit}>
+          <input
+            type="text"
+            placeholder="City..."
+            onChange={(e) => setLocsQuery(e.target.value)}
+          />
+        </form>
+      )}
 
-    {isLoading && <p>Loading...</p>}
-    {locsArr && locsArr.map((arr) => {
-      return (<p>{arr[0]}:{arr[1]}, {arr[2]} </p>)
-    })}
+      {isLoading && <p>Loading...</p>}
 
+      {locsArr && (
+        <form>
+          <select name="finalLoc" id="finalLoc" onChange={handleLocChange}>
+            <option selected disabled>SELECT</option>
+            {locsArr &&
+              locsArr.map((arr) => {
+                return <option value={arr[0]}>{arr[0]}</option>;
+              })}
+          </select>
+          <button onClick={handleLocSubmit}>Submit</button>
+        </form>
+      )}
+      {/* {finalLoc && <p>finalLoc: {finalLoc}</p>} */}
     </div>
-  )
+  );
 }
 
-export default WeatherCharts
+export default WeatherCharts;
