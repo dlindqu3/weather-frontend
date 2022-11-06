94e81aa5c633876608ae5f4be2605428313868da
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-11-06 17:06:09 -0500

    can add user to db with signup from frontend

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,13 +1,33 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 function SignUp() {
 
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+  const [signupError, setSignupError] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+
+  const signup = async (username, password) => {
+    // setIsLoading(true)
+    let baseURL = `http://localhost:4500/api/user/signup`
+    let reqBody = {
+      username: username,
+      password: password
+    }
+
+    try {
+      const res = await axios.post(baseURL, reqBody)
+      console.log('new user: ', res.data.username, 'new token: ', res.data.token)
+    } catch (error){
+      setSignupError(error.response.data.error)
+    }
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log(username, password)
+    setSignupError("")
+    signup(username, password)
   }
 
   return (
@@ -15,6 +35,7 @@ function SignUp() {
       <p>Sign Up Page</p>
       <form onSubmit={handleSubmit}>
         <h4>Sign Up</h4>
+        {signupError && <p>{signupError}</p>}
         <label>Username:</label>
         <input 
           type="text"
