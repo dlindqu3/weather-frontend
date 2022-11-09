df36096c3ac33a16d351bf61bf733773ed53317b
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-11-09 16:32:05 -0500

    added tailwind to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,6 +7,8 @@ function Login({ setCurrentUser }) {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const [loginError, setLoginError] = useState(null)
+  const [displayPassword, setDisplayPassword] = useState(false);
+  const [passwordType, setPasswordType] = useState("password");
 
   let navigate = useNavigate()
 
@@ -40,30 +42,71 @@ function Login({ setCurrentUser }) {
 
   }
 
+  const handlePasswordDisplay = async () => {
+    console.log("handlePasswordDisplay clicked");
+    if (displayPassword === true) {
+      setDisplayPassword(false);
+      setPasswordType("password");
+    } else if (displayPassword === false) {
+      setDisplayPassword(true);
+      setPasswordType("text");
+    }
+  };
+
   return (
     <div>
-      <p>Login Page</p>
-      <form onSubmit={handleSubmit}>
-        <h4>Login</h4>
-        <label>Username:</label>
-        <input 
-          type="text"
-          onChange={(e) => {setUsername(e.target.value)}}
-          value={username}
-          className="text-black rounded-sm"
-        />
-        <br />
-        <label>Password:</label>
-        <input 
-          type="password"
-          onChange={(e) => {setPassword(e.target.value)}}
-          value={password}
-          className="text-black rounded-sm"
-        />
-        <br />
-        <button className="bg-blue-400 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">Submit</button>
-      </form>
+    <br />
+    <br />
+    <div className="flex justify-center">
+      <div className="w-full max-w-sm px-4 py-4 rounded border-solid border-2 border-slate-100">
+        <form onSubmit={handleSubmit}>
+          <h4 className="flex justify-center">Login</h4>
+          <label className="flex justify-center">Username:</label>
+          <div className="flex justify-center">
+            <input
+              type="text"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              value={username}
+              className="text-black rounded-sm my-1 flex justify-center"
+            />
+          </div>
+          <label className="flex justify-center">Password:</label>
+          <div className="flex justify-center">
+            <input
+              type={passwordType}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              value={password}
+              className="text-black rounded-sm my-1 flex justify-center"
+            />
+          </div>
+          <div className="flex justify-center">
+            <input
+              type="checkbox"
+              onChange={() => {
+                handlePasswordDisplay();
+              }}
+              className="mx-1"
+            />
+            {displayPassword && <span>Hide password</span>}
+            {!displayPassword && <span>Show password</span>}
+          </div>
+
+          <div className="flex justify-center">
+            {loginError ? <p>**{loginError}</p> : <p className="my-1"></p>}
+          </div>
+          <div className="flex justify-center">
+            <button className="bg-blue-400 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded my-2 flex justify-center">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
+  </div>
   )
 }
 
