c4d662461100fdbf0396538d75e08d22f47ef4cd
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-11-12 18:04:43 -0500

    changed about page text

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <div>
       <h3 className="flex justify-center">About</h3>
-      <p>This site was created as a personal project by Dwight Lindquist. The main tools used include MongoDB, Express, React, Node, Axios, JWT tokens, and bcrypt. Feel free to connect on GitHub or LinkedIn in the links below. Cheers.</p>
+      <p>This site was created as a personal project by Dwight Lindquist. The main tools used include MongoDB, Express, React, Node, Axios, JWT tokens, bcrypt, and Jest. Feel free to connect on GitHub or LinkedIn in the links below. Cheers.</p>
     </div>
   )
 }
